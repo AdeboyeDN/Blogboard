fb7bb32ba9929d1b610c2451c3f4def137c64c4e
Daniel <adeboyed93@gmail.com>
2022-09-05 11:28:27 +0300

    new

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   console.log(process.env.NEXT_PUBLIC_KEY)
+  console.log(process.env.NEXT_PUBLIC_NEW)
   return (
     <div className={styles.container}>
       <Head>
